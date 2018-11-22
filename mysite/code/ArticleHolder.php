d16e023ec055b962eba513cd4a623ff308d52cce
Robbie Averill <robbie@silverstripe.com>
2018-11-22 12:52:38 +0100

    Update composer.lock and fix query bug in ArticleHolder::ArchiveDates

--- mysite/code/ArticleHolder.php
+++ mysite/code/ArticleHolder.php
@@ -34,32 +34,25 @@ class ArticleHolder extends Page {
 
 	public function ArchiveDates() {
 		$list = ArrayList::create();
-		$stage = Versioned::current_stage();		
 
-		$query = new SQLQuery(array ());
-		$query->selectField("DATE_FORMAT(`Date`,'%Y_%M_%m')","DateString")
-			  ->setFrom("ArticlePage_{$stage}")
-			  ->setOrderBy("Date", "ASC")
-			  ->setDistinct(true);
+		$pages = ArticlePage::get()
+            ->sort('Date', 'ASC')
+            ->distinct(true);
 
-		$result = $query->execute();
-		
-		if($result) {
-			while($record = $result->nextRecord()) {
-				list($year, $monthName, $monthNumber) = explode('_', $record['DateString']);
+        foreach ($pages as $page) {
+            list($year, $monthName, $monthNumber) = explode('-', $page->Date);
 
-				$list->push(ArrayData::create(array(
-					'Year' => $year,
-					'MonthName' => $monthName,
-					'MonthNumber' => $monthNumber,
-					'Link' => $this->Link("date/$year/$monthNumber"),
-					'ArticleCount' => ArticlePage::get()->where("
-							DATE_FORMAT(`Date`,'%Y%m') = '{$year}{$monthNumber}'
-							AND ParentID = {$this->ID}
-						")->count()
-				)));
-			}
-		}
+            $list->push(ArrayData::create(array(
+                'Year' => $year,
+                'MonthName' => $monthName,
+                'MonthNumber' => $monthNumber,
+                'Link' => $this->Link("date/$year/$monthNumber"),
+                'ArticleCount' => ArticlePage::get()
+                    ->filter('ParentID', $this->ID)
+                    ->where("DATE_FORMAT(`Date`,'%Y%m') = '{$year}{$monthNumber}'")
+                    ->count()
+            )));
+        }
 
 		return $list;
 	}
@@ -120,7 +113,7 @@ class ArticleHolder_Controller extends Page_Controller {
 			'SelectedRegion' => $region
 		);
 	}
-	
+
 	public function date(SS_HTTPRequest $r) {
 		$year = $r->param('ID');
 		$month = $r->param('OtherID');
@@ -128,20 +121,20 @@ class ArticleHolder_Controller extends Page_Controller {
 		if(!$year) return $this->httpError(404);
 
 		$startDate = $month ? "{$year}-{$month}-01" : "{$year}-01-01";
-		
+
 		if(strtotime($startDate) === false) {
 			return $this->httpError(404, 'Invalid date');
-		} 
+		}
 
 		$adder = $month ? '+1 month' : '+1 year';
 		$endDate = date('Y-m-d', strtotime(
-						$adder, 
+						$adder,
 						strtotime($startDate)
 					));
 
 		$this->articleList = $this->articleList->filter(array(
 			'Date:GreaterThanOrEqual' => $startDate,
-			'Date:LessThan' => $endDate 
+			'Date:LessThan' => $endDate
 		));
 
 		return array (
@@ -152,7 +145,7 @@ class ArticleHolder_Controller extends Page_Controller {
 	}
 
 
-	public function PaginatedArticles ($num = 10) {		
+	public function PaginatedArticles ($num = 10) {
 		return PaginatedList::create(
 			$this->articleList,
 			$this->getRequest()
